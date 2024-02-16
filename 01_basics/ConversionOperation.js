4ae78ac4f90011e52ad4ab51644f95557d4e7601
Saurabh Yadav <96307895+SaurabhYdv2003@users.noreply.github.com>
2024-02-16 09:29:40 +0000

    Conversion

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,26 @@
+let score='33'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber= Number(score);
+
+// console.log(typeof(valueInNumber));
+
+//"33"=> 33
+// "33abc"=> NaN
+// true => 1;false=>0
+
+let isLoggedIn=1;
+
+let booleanIsLogeedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLogeedIn);
+
+// 1=> true; 0=>false
+//""=> false
+//"Saurabh"=> true
+
+let someNumber=33
+
+let stringNumber= String(someNumber);
+console.log(typeof(someNumber))
