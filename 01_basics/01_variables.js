3b7f4a5f00b92f295049f9edbe0412e8195cc1a6
Saurabh Yadav <96307895+SaurabhYdv2003@users.noreply.github.com>
2024-02-16 09:27:40 +0000

    Datatypes and variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID= 14555;
+let accountEmail= "Saurabhkumar22410@gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+let accountState;
+
+// accountId=2 // not allowed
+
+accountEmail="hc@hc.com";
+acountPassword="2121212121";
+accountCity="bengaluru";
+
+// console.log(accountId)
+
+/*
+Prefer not to use Var
+because of issue in block scope and functional scope
+
+*/
+
+console.log([accountID,accountEmail,accountPassword,accountCity,accountState])
